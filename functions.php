dff921f9a481e405accabec77ffee591232c0190
alvandwp <alvand1399@gmail.com>
2025-01-15 14:54:46 +0330

    feat: Registering a custom template part area for sidebars.

--- functions.php
+++ functions.php
@@ -1,6 +1,24 @@
 <?php
 /**
- * AlvandBlog functions and definitions
+ * Alvand Blog functions and definitions
  *
- * @package AlvandBlog
+ * @package Alvand Blog
  */
+
+/**
+ * Registering a custom template part area for sidebars.
+ *
+ * @param array $areas The template part areas.
+ */
+function alvandwp_template_part_areas( array $areas ) {
+	$areas[] = array(
+		'area'        => 'sidebar', // Unique identifier for the area.
+		'area_tag'    => 'aside',
+		'label'       => esc_html__( 'Sidebar', 'alvandblog' ), // Human-readable label.
+		'description' => esc_html__( 'A template part area for sidebars.', 'alvandblog' ), // Description of the area.
+		'icon'        => 'sidebar',
+	);
+
+	return $areas;
+}
+add_filter( 'default_wp_template_part_areas', 'alvandwp_template_part_areas' );
